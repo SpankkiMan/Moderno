6658c4531d9ec57a610fd466511b041a4390412b
SpankkiMan <www.samat.isen@mail.ru>
2021-11-25 16:54:01 +0400

    start1

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
     gulp.watch('app/*.html', gulp.parallel('html'))
 });
